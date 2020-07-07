dd98bfece0bdc98727c7c96aa7205d5195e394ad
Victor Alberto Amigo <amigo.victor.97@gmail.com>
2020-07-07 23:24:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,8 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-
+  monopoly = {:railroads => Hash.new}
+  monopoly
 end
 
 def monopoly_with_second_tier
@@ -9,7 +10,13 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  monopoly = {:railroads => Hash.new}
+  monopoly[:railroads] = {
+    :pieces => 4, 
+    :rent_in_dollars => Hash.new, 
+    :names => Hash.new
+  }
+  monopoly
 end
 
 def monopoly_with_third_tier
@@ -17,7 +24,21 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  monopoly_with_second_tier
+  hash[:railroads][:rent_in_dollars] = {
+    :one_piece_owned => 25, 
+    :two_pieces_owned => 50, 
+    :three_pieces_owned => 100, 
+    :four_pieces_owned => 200
+    
+  }
+  hash[:railroads][:names] = {
+    :reading_railroad => Hash.new, 
+    :pennsylvania_railroad => Hash.new, 
+    :b_and_o_railroad => Hash.new, 
+    :shortline_railroad => Hash.new
+  }
+  hash
 end
 
 def monopoly_with_fourth_tier
@@ -25,5 +46,18 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  #monopoly_with_third_tier
+  #hash[:railroads][:names][:reading_railroad] = {
+  #  "mortgage value" => "100"
+  #}
+  #hash[:railroads][:names][:pennsylvania_railroad] = {
+  #  "mortgage value" => "200"
+  #}
+  #hash[:railroads][:names][:b_and_o_railroad] = {
+  #  "mortgage value" => "400"
+  #}
+  #hash[:railroads][:names][:shortline_railroad] = {
+  #  "mortgage value" => "800"
+  #}
+  #hash
 end
